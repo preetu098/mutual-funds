6c43c2b9faf5610053bb01962453badaac06d44b
Preetu098 <preetu098@gmail.com>
2022-06-24 16:20:05 +0530

    bondpagedone

--- resources/views/pms.blade.php
+++ resources/views/pms.blade.php
@@ -5,6 +5,11 @@
         transition:all 5s;
       border:0px;
     }
+    #pms:hover 
+    {
+        transform:scale(0.9);
+        transition:all 3s;
+    }
 #cardhover:hover 
 {
 
@@ -88,7 +93,7 @@ box-shadow:2px 2px 2px 2px black;
                     </form>
                 </div> 
                 </div>
-            </section>
+        
             </div>
 
 
@@ -181,7 +186,119 @@ box-shadow:2px 2px 2px 2px black;
             </div>
         </div>
     </section>
-    
+    <section class="mt-5">
+    <div class="container">
+        <div class="row">
+            <center>
+                <h2 style="color:black">Our Philosophy</h2>
+                <h3 style="color:black">In EQUITY INVESTMENTS Performance depends on simply 2 factors</h3>
+            </center>
+        </div>
+        <div class="row" >
+           
+           <div class="col-lg-6">
+            <h3 class="text-info">Investing in Quality Portfolio</h3>
+            <hr>
+            <h4>A quality portfolio is the one that is not too much diversified, & still offers the best risk-adjusted returns. With us, this is identified through unbiased selection based on our proprietary analytics. It’s easy to socialize and sell products, which is what most wealth managers do. We’re reshaping the wealth management industry by sticking to basics, in-depth analytics. We maintain insight + integrity and aim at the long term prosperity of clients.</h4>
+           </div>
+           <div class="col-lg-6">
+           <h3 class="text-info">Holding it for Long Term</h3>
+            <hr>
+            <h4>
+            It is easy to aspire for a long term horizon when making an investment, but practically very difficult to maintain. With us, this is achieved through our educative super simple & articulate proprietary content. Holding on to the portfolio requires high conviction in the portfolio at all points of time, else, one tends to exit early owing to the traps of emotions or impatience.  We ensure our clients make informed investment decisions.
+            </h4>
+           </div>
+        </div>
+    </div>
+</section>
+<section>
+    <div class="container">
+        <div class="row">
+      
+                <h2 style="color:black" class="mt-5">Why Us</h2>
+
+        </div>
+        <div class="row">
+            <table>
+                <tr>
+                    <td>
+                <img src="https://i.dlpng.com/static/png/7046530_preview.png"/>
+                <h5>FOCUSED &
+CONCENTRATED
+PORTFOLIOS</h5>
+                    </td>
+                    <td>
+                    <img src="https://media.istockphoto.com/vectors/group-of-friends-holding-hands-jumping-vector-id165768414?k=20&m=165768414&s=170667a&w=0&h=RW89zC3EKgA1ZzM1Ky7Z4ANf1nrXPZZxYhsX0swUa40="/>
+                    <h5 class="mt-5">HIGH
+PERFORMANCE
+PHILOSOPHIES</h5>
+                        </td>
+                        <td>
+                        <img src="https://www.mishainfotech.com/images/hrms-im.jpg"/>
+                        <h5 class="mt-5">ADEPT
+PORTFOLIO
+MANAGERS</h5>
+                        </td>
+                        <td>
+                        <img src="https://www.kapturecrm.com/images/payments/payments-collection/Banner%20Img.svg"/>
+                        <h5 class="mt-5">ZERO
+SET UP
+FEES</h5>
+                        </td>
+                        <td>
+                        <img src="https://cdn-scripbox-wordpress.scripbox.com/wp-content/uploads/2020/03/advantages-of-mutual-funds-vector.png"/>
+                        <h5 class="mt-5">MINIMAL
+EXIT LOAD
+STRUCTURES</h5>
+                        </td>
+                        <td>
+                        <img src="https://www.counterpath.com/wp-content/uploads/2019/10/01-Softphone-1.jpg"/>
+                        <h5>BEST IN CLASS
+INVESTMENT SERVICE</h5>
+                        </td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</section>
+<section>
+        <div class="container mt-5">
+        
+          
+                <div class="row">
+                    <div class="col-md-8" >
+                   <img id="pms" src='https://abakkusinvest.com/wp-content/themes/twentytwenty/assets/images/new-ban.png' style="height:400px;"/>
+                    </div>
+                    <div class="col-md-4">
+                        <h3 class="" style="color:teal">Why & How Portfolio Management Services (PMS)</h3>
+                        <p>PMSs focusses on a few selected companies and says no to the rest, whereas Mutual funds are made to follow too diversified approach because these are products meant for the masses. Though this helps mutual funds in reducing volatility to an extent, but more doesn’t always mean low risk, and in fact, exposure to more companies may increase the risk of buying less known.</p>
+                    </div>
+                </div>
+            </div>
+         
+        </div>
+</section>
+<section class="mt-5">
+        <div class="container mt-5">
+         
+           <div>
+                <div class="row">
+                    <div class="col-md-4">
+                    <h3 class="" style="color:teal">Why and how Alternative Investment Funds(AIF)</h3>
+                        <p>AIFs combines the operational ease of a mutual fund and the flexibility of a PMS making it a perfect blend geared for generating optimum performance for a stipulated investment objective.
+
+ 
+
+To enhance risk-adjusted performance, these products can use complex strategies like unlisted equity investments, long-short hedging style of investments etc</p>
+                    </div>
+                    <div id="pms" class="col-md-8" style="height:300px;width:100%;background-image:url('https://miro.medium.com/max/624/1*A1_AbCVvlu5gX2giYpnCvQ.png');">
+                        
+                    </div>
+                </div>
+            </div>
+         
+        </div>
+</section>
      <!-- Start Faqs -->
      <section class="section-padding loan-faqs-sec bg-light-white findrate-top" style="margin-top:200px">
         <div class="container">
@@ -208,9 +325,11 @@ box-shadow:2px 2px 2px 2px black;
             </div>
             
         </div>
-
+</div>
     </section>
+    
     <!-- End Faqs -->
+
     <!-- Start Footer -->
     <footer class="bg-black section-padding footer">
         <div class="container">
